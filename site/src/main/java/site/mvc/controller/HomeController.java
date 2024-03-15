4473fd87088cb0a7d7fc2c07e922075b83bf0f0e
benchel <whalswl1030@nate.com>
2024-03-15 21:36:24 +0900

    게시판 홈 추가

---
+++ site/src/main/java/site/mvc/controller/HomeController.java
@@ -0,0 +1,15 @@
+package site.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/*")
+public class HomeController {
+
+	@GetMapping("/")
+	public String homeHTML() throws Exception {
+		return "site/home/index";
+	}
+}
