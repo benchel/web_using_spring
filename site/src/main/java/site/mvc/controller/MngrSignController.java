4497a6888f12f13874d110c395008a6debefc1dd
benchel <whalswl1030@nate.com>
2024-07-26 22:34:51 +0900

    관리자 로그인 화면 추가

---
+++ site/src/main/java/site/mvc/controller/MngrSignController.java
@@ -0,0 +1,15 @@
+package site.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mngr/sign")
+public class MngrSignController {
+
+	@GetMapping("/in")
+	public String sign_in() throws Exception {
+		return "mngr/sign/in";
+	}
+}
