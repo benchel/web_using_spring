0c1f9dd5f30c2af829a9912a0eb27ff29721f2d4
benchel <whalswl1030@nate.com>
2024-12-13 20:46:35 +0900

    value에 style property placeholder 적용

--- site/src/main/java/site/common/config/JasyptConfig.java
+++ site/src/main/java/site/common/config/JasyptConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
-	@Value("jasypt.encryptor.pswd")
+	@Value("${jasypt.encryptor.pswd}")
 	private String encrptKey;
 	
 	@Bean
