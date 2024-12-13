ee221c14139384144f758749be30812489d8804e
benchel <whalswl1030@nate.com>
2024-12-13 23:57:46 +0900

    jasypt 설정 변경

--- site/src/main/java/site/common/config/JasyptConfig.java
+++ site/src/main/java/site/common/config/JasyptConfig.java
@@ -1,7 +1,7 @@
 package site.common.config;
 
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.jasypt.iv.RandomIvGenerator;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +10,20 @@ import org.springframework.context.annotation.Configuration;
 public class JasyptConfig {
 
 	@Value("${jasypt.encryptor.pswd}")
-	private String encrptKey;
-	
+	private String encryptKey;
+    
 	@Bean("jasyptEncryptor")
-	public StandardPBEStringEncryptor standardPBEStringEncryptor() {
-		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-		encryptor.setPassword(encrptKey);
-		encryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
-		encryptor.setIvGenerator(new RandomIvGenerator());
-		return encryptor;
+	public SimpleStringPBEConfig jasyptEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword(encryptKey);
+		config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘 default
+		config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수 default
+		config.setPoolSize("1"); // 인스턴스 pool default
+		config.setProviderName("SunJCE"); // default
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
+		config.setStringOutputType("base64"); //인코딩 방식
+		encryptor.setConfig(config);
+		return config;
 	}
 }
