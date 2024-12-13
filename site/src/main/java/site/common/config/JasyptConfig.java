9ada6ee99985bb060f765def430b682ef7706de0
benchel <whalswl1030@nate.com>
2024-12-13 20:32:44 +0900

    jasypt 설정 변경

--- site/src/main/java/site/common/config/JasyptConfig.java
+++ site/src/main/java/site/common/config/JasyptConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
-	@Value("jasypt.encryptor.psd")
+	@Value("jasypt.encryptor.pswd")
 	private String encrptKey;
 	
 	@Bean
