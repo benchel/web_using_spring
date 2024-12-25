fa8e30d9d2ec86e100d24b68823e8364fae7ff99
benchel <whalswl1030@nate.com>
2024-12-26 00:09:01 +0900

    jasypt 설정

--- site/src/main/java/site/common/config/JasyptConfig.java
+++ site/src/main/java/site/common/config/JasyptConfig.java
@@ -1,29 +1,34 @@
 package site.common.config;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @Configuration
+@EnableEncryptableProperties
 public class JasyptConfig {
 
 	@Value("${jasypt.encryptor.pswd}")
 	private String encryptKey;
     
-	@Bean("jasyptEncryptor")
-	public SimpleStringPBEConfig jasyptEncryptor() {
-		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		config.setPassword(encryptKey);
-		config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘 default
-		config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수 default
-		config.setPoolSize("1"); // 인스턴스 pool default
-		config.setProviderName("SunJCE"); // default
-		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
-		config.setStringOutputType("base64"); //인코딩 방식
-		encryptor.setConfig(config);
-		return config;
-	}
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(encryptKey);
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
 }
