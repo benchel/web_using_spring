edb7694454d4486551cb0838e4f7c10b9a5f9952
benchel <whalswl1030@nate.com>
2025-06-07 06:18:11 +0900

    csrf 토큰 위치변경

---
+++ site/src/main/java/site/constant/SessionConstructor.java
@@ -0,0 +1,9 @@
+package site.constant;
+
+public class SessionConstructor {
+	public static final String SESSION_USER_ID = "ssUserId";
+	public static final String SESSION_USER_NAME = "ssUserName";
+	public static final String SESSION_USER_EMAIL = "ssUserEmail";
+	public static final String SESSION_USER_CONTACT = "ssUserContact";
+	public static final String SESSION_USER_AUTHORITY = "ssUserAuth";
+}
