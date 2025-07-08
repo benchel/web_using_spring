da3b36ab81610d8abafebca50f4fc3b93c470dce
benchel <whalswl1030@nate.com>
2025-07-08 13:10:09 +0900

    게시글 답글(5)

--- site/src/main/java/site/mvc/service/CommentService.java
+++ site/src/main/java/site/mvc/service/CommentService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
-import site.common.utill.Paging;
 import site.mvc.dto.CommentDTO;
 import site.mvc.mapper.CommentMapper;
 import site.mvc.vo.CommentVO;
@@ -51,32 +50,13 @@ public class CommentService {
 	public Map<String, Object> list(CommentDTO commentDTO) throws Exception {
 
 		Map<String, Object> result = new HashMap<>();
-		
-		int pageNo = commentDTO.getPageNo() == 0 ? 1 : commentDTO.getPageNo();
-		int pageSize = commentDTO.getPageSize() == 0 ? 10 : commentDTO.getPageSize();
-		int pageBlock = commentDTO.getPageBlock() == 0 ? 10 : commentDTO.getPageBlock();
-		commentDTO.setPageNo(pageNo);
-		commentDTO.setPageSize(pageSize);
-		commentDTO.setPageBlock(pageBlock);
-		commentDTO.setPageOffset(Paging.getPageOffset(pageNo, pageSize));
-		
+
 		List<CommentVO> list = commentMapper.list(commentDTO);
-		
-		if(pageNo != 1 && list.size() == 0) {
-			pageNo = 1;
-			commentDTO.setPageNo(pageNo);
-			commentDTO.setPageOffset(Paging.getPageOffset(pageNo, pageSize));
-			result.put("list", commentMapper.list(commentDTO));
-		}
-		
+
 		int totalCount = commentMapper.count(commentDTO);
-		int totalPageNo = Paging.getTotalPageNo(totalCount, pageSize);
-		String pagingHTML = Paging.getPagingHTML(totalCount, pageNo, pageSize, pageBlock);
 		
 		result.put("list", list);
 		result.put("totalCount", totalCount);
-		result.put("totalPageNo", totalPageNo);
-		result.put("pagingHTML", pagingHTML);
 		
 		return result;
 	}
