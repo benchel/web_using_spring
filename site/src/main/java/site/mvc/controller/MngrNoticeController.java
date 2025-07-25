39117ffaecdb61223a9b4ab7427ee8462184021d
benchel <whalswl1030@nate.com>
2025-07-26 08:19:50 +0900

    관리자-공지사항 목록

--- site/src/main/java/site/mvc/controller/MngrNoticeController.java
+++ site/src/main/java/site/mvc/controller/MngrNoticeController.java
@@ -1,15 +1,33 @@
 package site.mvc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.RequiredArgsConstructor;
+import site.mvc.dto.NoticeDTO;
+import site.mvc.service.NoticeService;
 
 @Controller
 @RequestMapping("/mngr/notice/*")
+@RequiredArgsConstructor
 public class MngrNoticeController {
 
+	private final NoticeService noticeService;
+	
 	@GetMapping("/list")
 	public String listHTML() throws Exception {
 		return "mngr/notice/list";
+	}
+	
+	@PostMapping("/list")
+	@ResponseBody
+	public ResponseEntity<?> list(@RequestBody NoticeDTO noticeDTO) throws Exception {
+		return new ResponseEntity<>(noticeService.list(noticeDTO), HttpStatus.OK);
 	}
 }
